b76e88774a41ceae10dbc6ac54249484f5519f9d
N1k1taN <111293823+N1k1taN@users.noreply.github.com>
2024-10-01 23:26:40 +0300

    asdsaddsa

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,12 @@
 import '../styles/App.css';
 import Navbar from '../components/navbar';
+import { Varela } from 'next/font/google';
+const varela = Varela({
+  weight: '400',
+  subsets: ['latin'],
+});
+
+
 
 export const metadata = {
   title: 'My Website',
@@ -8,8 +15,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body>
+    <html lang="ua">
+      <body className={varela.className}>
         <Navbar />
         <main>{children}</main>
       </body>
